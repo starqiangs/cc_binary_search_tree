23c2f7229aa2c8f6542be77a91fe0ab923f9031f
qiang <star.lvqiang@gmail.com>
2023-08-29 16:48:54 +0800

    add insert and pre in post print

--- src/include/BSTree.h
+++ src/include/BSTree.h
@@ -1,4 +1,8 @@
+#pragma once
+
 #include "BSTNode.h"
+
+// BST树的定义为 左子树节点小于父节点 父节点小于右子树节点
 
 template <typename T>
 class BSTree
@@ -6,9 +10,109 @@ class BSTree
 public:
     BSTree() : _root(nullptr){};
     ~BSTree(){};
-    bool Insert(T data);
-    bool Delete(T data);
-    void Print();
+    // 循环
+    bool Insert(BSTNode<T> *node)
+    {
+        BSTNode<T> *temp = nullptr;
+        BSTNode<T> *parent = nullptr;
+        if (this->_root == nullptr)
+        {
+            this->_root = node;
+            return true;
+        }
+        temp = this->_root;
+        while (temp != nullptr)
+        {
+            parent = temp;
+            if (temp->_data > node->_data)
+            {
+                temp = temp->_lchild;
+            }
+            else
+            {
+                temp = temp->_rchild;
+            }
+        }
+        // 判断该节点
+        if (node->_data < parent->_data)
+        {
+            parent->_lchild = node;
+        }
+        else
+        {
+            parent->_rchild = node;
+        }
+        return true;
+    }
+
+    // 递归
+    bool InsertNode(BSTNode<T> *tree, BSTNode<T> *node)
+    {
+        if (tree == nullptr)
+        {
+            tree = node;
+            return true;
+        }
+        if (tree->_data > node->_data)
+        {
+            return InsertNode(tree->_lchild, node);
+        }
+        else
+        {
+            return InsertNode(tree->_rchild, node);
+        }
+    }
+
+    BSTNode<T> *SearchNode(int data)
+    {
+        BSTNode<T> *temp = this->_root;
+        while (temp != nullptr)
+        {
+            if (temp->_data > data)
+            {
+                return SearchNode(temp->_lchild, data);
+            }
+            else if (temp->_data < data)
+            {
+                return SearchNode(temp->_rchild, data);
+            }
+            else
+            {
+                return temp;
+            }
+        }
+        return nullptr;
+    }
+
+    void InOrder(BSTNode<T> *tree)
+    {
+        if (tree == nullptr)
+            return;
+        InOrder(tree->_lchild);
+        std::cout << tree->_data << " ";
+        InOrder(tree->_rchild);
+    }
+    void PreOrder(BSTNode<T> *tree)
+    {
+        if (tree == nullptr)
+            return;
+        std::cout << tree->_data << " ";
+        PreOrder(tree->_lchild);
+        PreOrder(tree->_rchild);
+    }
+    void PostOrder(BSTNode<T> *tree)
+    {
+        if (tree == nullptr)
+            return;
+        PostOrder(tree->_lchild);
+        PostOrder(tree->_rchild);
+        std::cout << tree->_data << " ";
+    }
+
+    BSTNode<T> *GetRoot()
+    {
+        return _root;
+    }
 
 private:
     BSTNode<T> *_root;
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,27 @@
 #include <iostream>
 
+#include "BSTree.h"
+
 int main()
 {
+    BSTree<int> tree;
+
+    for (int i = 1; i < 8; i++)
+    {
+        BSTNode<int> *node = new BSTNode<int>(i, nullptr, nullptr);
+        tree.Insert(node);
+    }
+    std::cout << "Inorder traversal: ";
+    tree.InOrder(tree.GetRoot());
+    std::cout << std::endl;
+
+    std::cout << "Preorder traversal: ";
+    tree.PreOrder(tree.GetRoot());
+    std::cout << std::endl;
+
+    std::cout << "Postorder traversal: ";
+    tree.PostOrder(tree.GetRoot());
+    std::cout << std::endl;
+
     return 0;
 }
